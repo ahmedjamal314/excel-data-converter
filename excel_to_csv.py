5074df6f00a47bb820973992751be5e5b261faf5
ahmedjamal314 <ahmedjamal314@gmail.com>
2025-04-17 01:16:21 -0400

    Update excel_to_csv.py

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 def convert_excel_to_csv(excel_file_path, csv_file_path):
     df = pd.read_excel(excel_file_path)
     df.to_csv(csv_file_path, index=False)
-    print(f"✅ Excel converted to CSV successfully: {csv_file_path}")
+    print(f" Excel converted to CSV successfully: {csv_file_path}")
 
 convert_excel_to_csv(
     "C:/Users/ahmed/OneDrive/Desktop/QuickFix Vehicles Case Study Data.xlsx",
